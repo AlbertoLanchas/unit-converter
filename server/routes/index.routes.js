4debf8b47b8fca1be0af0ffe7ae9be293d41d483
AlbertoLanchas <albertolsn7@gmail.com>
2022-11-08 15:29:31 +0100

    Edit index routes files
    Signed-off-by: AlbertoLanchas <albertolsn7@gmail.com>

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -2,12 +2,4 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.json("pong");
-});
-
-router.get("/ping", (req, res) => {
-  res.json("ping");
-});
-
 export default router;
